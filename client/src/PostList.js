595c4bf46d54147ebeb9a60c2754b6d9618cfb26
Krzysztof Morawski <kmora@softserveinc.com>
2022-09-15 15:48:55 +0200

    comments feature added

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 const PostList = () => {
   const [posts, setPosts] = useState({});
@@ -22,6 +24,8 @@ const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
