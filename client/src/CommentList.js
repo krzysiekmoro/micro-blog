12393889c1934fe6f4c10559aaafd5c1a3f4c1ad
Krzysztof Morawski <kmora@softserveinc.com>
2022-09-16 12:08:25 +0200

    rerouted requests to event bus

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,20 +1,6 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchComments = async () => {
-    const response = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-    setComments(response.data);
-  };
-
-  useEffect(() => {
-    fetchComments();
-  }, []);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
