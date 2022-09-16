927f4dc96409015dd75a4b04af5db83b014dc6f3
Krzysztof Morawski <kmora@softserveinc.com>
2022-09-16 14:25:54 +0200

    comments moderation added to services

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,13 +9,20 @@ app.use(bodyParser.json());
 app.post("/events", (req, res) => {
   const event = req.body;
 
+  // Posts service
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
   });
+  // Comments service
   axios.post("http://localhost:4001/events", event).catch((err) => {
     console.log(err.message);
   });
+  // Query service
   axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  // Moderation service
+  axios.post("http://localhost:4003/events", event).catch((err) => {
     console.log(err.message);
   });
 
